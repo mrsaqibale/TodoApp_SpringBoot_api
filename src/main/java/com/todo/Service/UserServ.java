e58836fb85be6af90d56861ae1a47f270f3925b0
codewithprofessor <kalikali72547254@gmail.com>
2024-08-29 00:07:22 +0500

    update user servise

--- src/main/java/com/todo/Service/UserServ.java
+++ src/main/java/com/todo/Service/UserServ.java
@@ -1,5 +1,29 @@
 package com.todo.Service;
 
+import java.util.List;
+
+import com.todo.dto.UserDto;
+
 public interface UserServ {
 
+    // create user by admin or he is admin, or admin power
+	UserDto createUser(UserDto userDto);
+
+    // update profile by admin 
+    UserDto updateUser(UserDto userDto);
+
+    // show all the users 
+    List<UserDto>  getAllUsers();
+
+    // get user by id 
+    UserDto getUserById(Long id);
+
+    // soft del user 
+    void UserDelete(Long id);
+
+    // register user 
+
+    // login user
+	
+	
 }
