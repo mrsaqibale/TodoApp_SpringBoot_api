c7a11352fde26e43d97d8b247f90a8792ac36243
codewithprofessor <kalikali72547254@gmail.com>
2024-08-28 22:52:20 +0500

    add update dto

--- src/main/java/com/todo/dto/TodoDto.java
+++ src/main/java/com/todo/dto/TodoDto.java
@@ -1,5 +1,29 @@
 package com.todo.dto;
 
+import java.time.LocalDateTime;
+
+
 public class TodoDto {
 
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private boolean isCompleted;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+
+    private boolean isDeleted;
+
+    private int priority;
+
+    private String tag;
+
+    // private User user;
 }
--- src/main/java/com/todo/dto/UserDto.java
+++ src/main/java/com/todo/dto/UserDto.java
@@ -1,5 +1,32 @@
 package com.todo.dto;
 
+import java.time.LocalDateTime;
+
 public class UserDto {
 
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    private String phoneNumber;
+
+    private String profilePicture;
+    
+    private String role;
+
+    private LocalDateTime createdAt ;
+
+    private LocalDateTime updatedAt;
+
+    private boolean isLocked;
+
+    private boolean isDeleted;
+
+    private int loginCount;
+    
+    // private List<Todo> todo;
 }
