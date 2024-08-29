4187cb38dd5092f27be7b95e689fc02b71107c93
codewithprofessor <kalikali72547254@gmail.com>
2024-08-29 22:01:04 +0500

    create user lgoic of admin

--- src/main/java/com/todo/Service/imp/UserServImp.java
+++ src/main/java/com/todo/Service/imp/UserServImp.java
@@ -1,17 +1,39 @@
 package com.todo.Service.imp;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.todo.Service.UserServ;
 import com.todo.dto.UserDto;
+import com.todo.entites.User;
+import com.todo.repository.UserRepo;
 
 public class UserServImp implements UserServ{
 
-    
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private UserRepo userRepo;
+
+
+    // create user for the admin only with role of admin
     @Override
     public UserDto createUser(UserDto userDto) {
-
-        return null;
+        // set password encoder
+        User user = modelMapper.map(userDto, User.class);
+        user.setCreatedAt(LocalDateTime.now());
+        user.setUpdatedAt(LocalDateTime.now());
+        user.setRole("ADMIN");
+        user.setDeleted(false);
+        user.setLocked(false);
+        user.setLoginCount(0);
+        user.setProfilePicture("default.png");
+        User user2 = this.userRepo.save(user);
+        return this.modelMapper.map(user2, UserDto.class);
     }
 
     @Override
