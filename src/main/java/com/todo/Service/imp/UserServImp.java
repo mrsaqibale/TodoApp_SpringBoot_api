07855bebeb9128343a84608a664a27b7762dfe0a
codewithprofessor <kalikali72547254@gmail.com>
2024-08-29 20:57:17 +0500

    extend userserv

--- src/main/java/com/todo/Service/imp/UserServImp.java
+++ src/main/java/com/todo/Service/imp/UserServImp.java
@@ -1,5 +1,40 @@
 package com.todo.Service.imp;
 
-public class UserServImp {
+import java.util.List;
+
+import com.todo.Service.UserServ;
+import com.todo.dto.UserDto;
+
+public class UserServImp implements UserServ{
+
+    @Override
+    public UserDto createUser(UserDto userDto) {
+        
+        return null;
+    }
+
+    @Override
+    public UserDto updateUser(UserDto userDto) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateUser'");
+    }
+
+    @Override
+    public List<UserDto> getAllUsers() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllUsers'");
+    }
+
+    @Override
+    public UserDto getUserById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getUserById'");
+    }
+
+    @Override
+    public void UserDelete(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'UserDelete'");
+    }
 
 }
