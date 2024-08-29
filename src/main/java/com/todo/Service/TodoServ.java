3af0d1110c13b80c649687274a298e60a0854fef
codewithprofessor <kalikali72547254@gmail.com>
2024-08-29 20:48:57 +0500

    create controller

--- src/main/java/com/todo/Service/TodoServ.java
+++ src/main/java/com/todo/Service/TodoServ.java
@@ -2,4 +2,12 @@ package com.todo.Service;
 
 public interface TodoServ {
 
+    // create todo
+    // update todo
+    // delete todo of user
+    // get all todos of user
+    // get todo by id of user
+    // get complete todo
+    // get on priority todo 
+    // get todo by search of user
 }
