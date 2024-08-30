75fb86a85ddb3572546e98194728393a85dca197
codewithprofessor <kalikali72547254@gmail.com>
2024-08-30 22:07:48 +0500

    create chain of security

--- src/main/java/com/todo/security/SecurityConfig.java
+++ src/main/java/com/todo/security/SecurityConfig.java
@@ -1,5 +1,20 @@
 package com.todo.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
 public class SecurityConfig {
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(request -> request
+                .requestMatchers("/api/user/register" , "/api/user/login").permitAll()
+                .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
 }
