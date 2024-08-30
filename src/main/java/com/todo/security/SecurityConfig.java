86379cbae5df1bdaf4b08450ff11cc37b40e6df8
codewithprofessor <kalikali72547254@gmail.com>
2024-08-30 23:31:11 +0500

    authenticate from db

--- src/main/java/com/todo/security/SecurityConfig.java
+++ src/main/java/com/todo/security/SecurityConfig.java
@@ -1,14 +1,23 @@
 package com.todo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder.BCryptVersion;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+    // userdetailservise 
+    @Autowired
+    private UserDetailsService userDetailsService;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         return http
@@ -16,7 +25,16 @@ public class SecurityConfig {
             .authorizeHttpRequests(request -> request
                 .requestMatchers("/auth/**").permitAll()
                 .anyRequest().authenticated())
-                
+                .httpBasic(Customizer.withDefaults())
                 .build();
+    }
+
+    // if authenticate from database 
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(new BCryptPasswordEncoder(BCryptVersion.$2A,12));
+        provider.setUserDetailsService(userDetailsService);
+        return provider;
     }
 }
