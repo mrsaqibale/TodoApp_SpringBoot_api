1ca0ef9a164e673d602566d546aef39e551e39dd
codewithprofessor <kalikali72547254@gmail.com>
2024-08-30 10:47:55 +0500

    set global ex with error

--- src/main/java/com/todo/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/todo/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,13 @@
 package com.todo.exceptions;
 
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
     
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException e) {
+        String msg = e.getMessage();
+        return null;
 }
