d765517976fa0e4ba87561e8f4a6456f4be32cde
codewithprofessor <kalikali72547254@gmail.com>
2024-08-30 10:52:11 +0500

    exception response for resourse not found

---
+++ src/main/java/com/todo/payload/ExceptionResponse.java
@@ -0,0 +1,22 @@
+package com.todo.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExceptionResponse {
+    
+    private String msg;
+
+    private boolean flag;
+
+    private Long statusCode; 
+
+
+    
+}
