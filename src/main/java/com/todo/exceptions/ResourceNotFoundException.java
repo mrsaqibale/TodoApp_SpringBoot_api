a5826c3bae3b3011d3555d462a0df2cb58fadbc5
codewithprofessor <kalikali72547254@gmail.com>
2024-08-30 10:11:49 +0500

    exception if field not found

--- src/main/java/com/todo/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/todo/exceptions/ResourceNotFoundException.java
@@ -1,5 +1,26 @@
 package com.todo.exceptions;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException{
+    
+    private String resource ;
+
+    private String field ;
+
+    private Long id;
+
+    // if data not found in table 
+    public ResourceNotFoundException(String r, String f , Long id){
+        super(String.format("%s not found with %s: %s",r, f, id));
+        this.resource = r;
+        this.field = f;
+        this.id = id;
+    }
+
+    // if username not found in user at login
+    public ResourceNotFoundException(String r, String f ){
+        super(String.format("%s not found with username: %s",r, f));
+        this.resource = r;
+        this.field = f;
+    }
 
 }
