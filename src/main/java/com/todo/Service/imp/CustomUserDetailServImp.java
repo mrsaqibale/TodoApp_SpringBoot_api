a44db55793ad71d5885a7979a6ef54ea7b8ec4e1
codewithprofessor <kalikali72547254@gmail.com>
2024-08-30 23:41:41 +0500

    complete customuserservice

--- src/main/java/com/todo/Service/imp/CustomUserDetailServImp.java
+++ src/main/java/com/todo/Service/imp/CustomUserDetailServImp.java
@@ -1,5 +1,30 @@
 package com.todo.Service.imp;
 
-public class CustomUserDetailServImp {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.todo.entites.User;
+import com.todo.exceptions.ResourceNotFoundException;
+import com.todo.repository.UserRepo;
+
+@Service
+public class CustomUserDetailServImp implements UserDetailsService{
+
+	@Autowired
+	private UserRepo userRepo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        try {
+            User user = this.userRepo.findActiveByUsername(username);
+            return user;
+        } catch (Exception e) {
+            throw new ResourceNotFoundException("User", username);
+        }
+
+	}
 
 }
