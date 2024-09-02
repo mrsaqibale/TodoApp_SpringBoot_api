684bd610f560232b19e74e7fc8d6edf38230815d
codewithprofessor <kalikali72547254@gmail.com>
2024-09-02 22:26:55 +0500

    update service of todo

--- src/main/java/com/todo/Service/TodoServ.java
+++ src/main/java/com/todo/Service/TodoServ.java
@@ -1,9 +1,15 @@
 package com.todo.Service;
 
+import com.todo.dto.TodoDto;
+
 public interface TodoServ {
 
     // create todo
+    TodoDto createTodo(TodoDto TodoDto);
+
+
     // update todo
+    TodoDto updateDto(TodoDto todoDto);
     // delete todo of user
     // get all todos of user
     // get todo by id of user
