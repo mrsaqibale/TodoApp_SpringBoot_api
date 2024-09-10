a2c161bd8e0da55897ba2be1fba5fe56807b9aa0
codewithprofessor <kalikali72547254@gmail.com>
2024-09-10 23:10:26 +0500

    register comp api

--- src/main/java/com/todo/controller/UserLogCont.java
+++ src/main/java/com/todo/controller/UserLogCont.java
@@ -1,15 +1,25 @@
 package com.todo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.todo.Service.UserServ;
+import com.todo.dto.UserLog;
+import com.todo.entites.User;
 
 @RestController
 @RequestMapping("/auth")
 public class UserLogCont {
     
-    @PostMapping("/login")
-    public String login() {
-        return "login";
+    @Autowired
+    private UserServ userServ;
+
+    @PostMapping("/register")
+    public UserLog register(@RequestBody UserLog user) {
+        this.userServ.registerUser(user);
+        return user;
     }
 }
