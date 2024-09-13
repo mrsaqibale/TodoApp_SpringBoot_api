76569c36c6852ee83b14b6112f9e67d87adf0fd1
codewithprofessor <kalikali72547254@gmail.com>
2024-09-13 21:24:38 +0500

    todo repo udpate with methods

--- src/main/java/com/todo/repository/TodoRepo.java
+++ src/main/java/com/todo/repository/TodoRepo.java
@@ -6,4 +6,14 @@ import com.todo.entites.Todo;
 
 public interface TodoRepo extends JpaRepository<Todo, Long>{
 
+    // get todo of user with id uncomplete
+
+    // get all todo of user uncomplete
+
+    // get complete todo of user with id 
+
+    // get complete todo of user all 
+
+    // get todo of today 
+    
 }
