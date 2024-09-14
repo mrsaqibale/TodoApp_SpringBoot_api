bf1ae7439c6f19f4a8310aa78ecced588d0dd782
codewithprofessor <kalikali72547254@gmail.com>
2024-09-14 22:00:31 +0500

    create token imp

--- src/main/java/com/todo/jwt/JwtService.java
+++ src/main/java/com/todo/jwt/JwtService.java
@@ -1,15 +1,92 @@
 package com.todo.jwt;
 
-import org.springframework.cache.interceptor.KeyGenerator;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.Base64;
 
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Service
 public class JwtService {
-    private String finalKey;
 
-    public JwtService(){
-        try {
-            // KeyGenerator keyGen 
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
+     private String fkey;
+    public JwtService() {
+         try {
+         KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
+         SecretKey sk = keyGen.generateKey();
+         fkey = Base64.getEncoder().encodeToString(sk.getEncoded());
+         } catch (Exception e) {
+         throw new RuntimeException();
+         }
+        // fkey = "R{!D6,wY=i9vcjqMwR2P6JUNG8~v7r1cBI=?Z=uUg/!NN>&v?0j%}z,DQ6eEc&";
+         
+
     }
+
+    public String genrateToken(String username) {
+        Map<String, Object> claims = new HashMap<>();
+        return Jwts.builder()
+                .claims()
+                .add(claims)
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 60 * 60 * 10))
+                .and()
+                .signWith(getKey())
+                .compact();
+    }
+
+    private SecretKey getKey() {
+        byte[] keyBytes = Decoders.BASE64
+                .decode("thekeyvalidforthemanofthematchwhichisgoodforallthepersons");
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String extractUserName(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+    	Claims clm;
+    	clm = Jwts.parser()
+    			.verifyWith((SecretKey) getKey())
+    			.build()
+    			.parseSignedClaims(token)
+    			.getPayload();
+    	System.out.println("stage 4");
+    	return clm ;
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final String userName = extractUserName(token);
+        System.out.println("is token is valid" + (userName.equals(userDetails.getUsername()) && !isTokenExpired(token)));
+        return (userName.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+    	System.out.println("is token expired " + extractExpiration(token).before(new Date()));
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
 }
